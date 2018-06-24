4cd38c322cd009e5859f96a2045f89650669e85e
Chacko <chackop@gmail.com>
2018-06-24 01:09:18 +0100

    added icons. TODO rowheight

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    images: {
+        heartIcon: require('../assets/heart.png'),
+        rightIcon: require('../assets/rightarrow.png'),
+        chatIcon: require('../assets/chat.png'),
+    },
+    stylesConstants: {
+        rowHeight: 50,
+    },
+};
